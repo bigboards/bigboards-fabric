e11aca596f58e9bd5783b0e02d332e8997153a66
calmera <Agm|kg1&>
2016-04-01 11:08:56 +0200

    removed deprecated shell scripts

--- consul.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IFACE=wlp2s0
-IP=$(ifconfig $IFACE |grep "inet addr:" | awk '{ print $3}' |cut -d ':' -f2)
-ID=$(ifconfig $IFACE |grep "HWaddr " | awk '{ print tolower($5)}' | tr -d ':')
-
-#TAGS="-tag hex-name=dev -tag hex-id=dev -tag arch=x86_64 -tag role=master"
-
-./lib/consul/consul-$(uname -m) agent -dev -dc lab -node $ID -advertise $IP -ui
--- deploy_to.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-HEX_NAME=$1
-
-scp -r ./src/main/* bb@${HEX_NAME}.hex.bigboards.io:/opt/bb/runtimes/bigboards-mmc/
-scp -r ./package.json bb@${HEX_NAME}.hex.bigboards.io:/opt/bb/runtimes/bigboards-mmc/
-ssh bb@${HEX_NAME}.hex.bigboards.io 'cd /opt/bb/runtimes/bigboards-mmc/ && npm install'
-ssh bb@${HEX_NAME}.hex.bigboards.io 'cd /opt/bb/runtimes/bigboards-mmc/client && bower install'
-ssh bb@${HEX_NAME}.hex.bigboards.io 'bb mmc restart'
